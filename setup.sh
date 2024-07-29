e6a9dd7c2ef9bf76dc980b65c8d99cf9cf82847c
Daniel Chung <ms0227162@gmail.com>
2024-07-29 11:57:59 +0000

    add tig

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # Some useful tools
 sudo update && sudo upgrade
-sudo apt install terminator tmux vim mc
-
+sudo apt install terminator tmux vim mc tig
 
 # Vim setup
+# See more command on https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
